6084840d36a5f904bf1b58dd02d9d50dae6b5cac
Namiki1520 <jairocsnamiki@hotmail.com>
2025-03-01 19:11:25 -0300

    Retornando as configurações do redis

--- support/redis.js
+++ support/redis.js
@@ -10,17 +10,10 @@ const queueName = 'twoFactorQueue'
 const queue = new Queue(queueName, { connection })
 
 export const getJob = async () => {
-    const jobs = await queue.getJob(["waiting", "delayed", "active"]);
-
-    if (!jobs.length) {
-        console.warn("⚠ Nenhum job encontrado na fila.");
-        return null; // Retorna null para evitar erro
-    }
-
-    console.log("✅ Job encontrado:", jobs[0].data);
-    return jobs[0].data.code; // Retorna apenas o código 2FA
-};
+    const jobs = await queue.getJobs();
+    return jobs[0].data.code
+}
 
 export const cleanJobs = async () => {
-    await queue.obliterate({ force: true })
+    await queue.obliterate()
 }
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -21,28 +21,31 @@ test('Nao deve logar quando o codigo de autenticacao e invalido', async ({ page 
 });
 
 test('Deve acessar a conta do usuário pegando codigo redis', async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  const dashbPage = new DashbPage(page);
-
+  const loginPage = new LoginPage(page)
+  const dashPage = new DashPage(page)
+  
   const usuario = {
     cpf: '00000014141',
-    senha: '147258',
-  };
-
+    senha: '147258'
+  }
+  
   await cleanJobs()
 
-  await loginPage.acessaPagina();
-  await loginPage.preencheCPF(usuario.cpf);
-  await loginPage.preencheSenha(usuario.senha);
+  await loginPage.acessaPagina()
+  await loginPage.informaCpf(usuario.cpf)
+  await loginPage.informaSenha(usuario.senha)
 
-  const codigo = await getJob();
+  // checkpoint
+  await page.getByRole('heading', {name: 'Verificação em duas etapas'})
+    .waitFor({timeout: 3000})
 
-  await page.getByRole('heading', { name: 'verificação em duas etapas' })
-    .waitFor({ timeout: 3000 });
-    
-  await loginPage.preencheCodigo2FA(codigo);
+  const codigo = await getJob()
 
-  await expect(await dashbPage.obterSaldo()).toHaveText('R$ 5.000,00');
+  // const codigo = await obterCodigo2FA(usuario.cpf)
+
+  await loginPage.informe2FA(codigo)
+
+  await expect(await dashPage.obterSaldo()).toHaveText('R$ 5.000,00')  
 });
 
 test('Deve acessar a conta do usuário pegando código do banco', async ({ page }) => {
