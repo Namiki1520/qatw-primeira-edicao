eef4ede687bf1293ff4c0dbd6a80e2dce7880101
Namiki1520 <jairocsnamiki@hotmail.com>
2025-03-01 18:49:07 -0300

    Adicionando testes com redis

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -20,30 +20,30 @@ test('Nao deve logar quando o codigo de autenticacao e invalido', async ({ page 
   await expect(page.locator('span')).toContainText('Código inválido. Por favor, tente novamente.');
 });
 
-// test('Deve acessar a conta do usuário pegando codigo redis', async ({ page }) => {
-//   const loginPage = new LoginPage(page);
-//   const dashbPage = new DashbPage(page);
+test('Deve acessar a conta do usuário pegando codigo redis', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  const dashbPage = new DashbPage(page);
 
-//   const usuario = {
-//     cpf: '00000014141',
-//     senha: '147258',
-//   };
+  const usuario = {
+    cpf: '00000014141',
+    senha: '147258',
+  };
 
-//   //await cleanJobs() // não consigo usar o método
+  await cleanJobs()
 
-//   await loginPage.acessaPagina();
-//   await loginPage.preencheCPF(usuario.cpf);
-//   await loginPage.preencheSenha(usuario.senha);
+  await loginPage.acessaPagina();
+  await loginPage.preencheCPF(usuario.cpf);
+  await loginPage.preencheSenha(usuario.senha);
 
-//   const codigo = await getJob();
+  const codigo = await getJob();
 
-//   await page.getByRole('heading', { name: 'verificação em duas etapas' })
-//     .waitFor({ timeout: 3000 });
+  await page.getByRole('heading', { name: 'verificação em duas etapas' })
+    .waitFor({ timeout: 3000 });
     
-//   await loginPage.preencheCodigo2FA(codigo);
+  await loginPage.preencheCodigo2FA(codigo);
 
-//   await expect(await dashbPage.obterSaldo()).toHaveText('R$ 5.000,00');
-// });
+  await expect(await dashbPage.obterSaldo()).toHaveText('R$ 5.000,00');
+});
 
 test('Deve acessar a conta do usuário pegando código do banco', async ({ page }) => {
   const loginPage = new LoginPage(page);
